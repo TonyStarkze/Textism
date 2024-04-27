610d3b0133c4b9ae4176425bddf72b73470a891b
awpt1mus <kamehamehabeech@gmail.com>
2024-04-27 13:12:33 +0530

    add redis

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,28 @@
-import "./ensure.storage";
+// import "./ensure.storage";
+import "dotenv/config";
 import express from "express";
 import path from "path";
-import { parsePort } from "./utils";
-import { jsonRepository } from "./repository";
 import { engine } from "express-handlebars";
+import { ICache, Repository } from "./repository";
+import { createClient } from "redis";
+import { parsePort } from "./utils";
 
 const app = express();
+
+const redisClient = createClient({
+  username: process.env.CACHE_USER,
+  password: process.env.CACHE_PASS,
+  socket: {
+    host: process.env.CACHE_HOST,
+    port: +process.env.CACHE_PORT!,
+  },
+});
+
+redisClient
+  .on("error", (err) => console.error(`Redis client encountered error ${err}`))
+  .connect();
+
+const repository = new Repository(redisClient as any as ICache);
 
 //this sets handlebars template ending as render engine.
 app.engine("handlebars", engine());
@@ -23,46 +40,44 @@ app.use(express.json());
 
 //Homepage
 app.get("/", (req, res) => {
-    return res.render("index", { csspath: "/css/style.css" });
+  return res.render("index", { csspath: "/css/style.css" });
 });
 
 //Page where user creates text to share
 app.get("/sender", (req, res) => {
-    return res.render("sender", { csspath: "/css/sender.css" });
+  return res.render("sender", { csspath: "/css/sender.css" });
 });
-
 
 //handles post request from the form.
 app.post("/sender", async (req, res) => {
-    const { text } = req.body;
+  const { text } = req.body;
 
-    if (!text) {
-        return res.render("sender", { csspath: "/css/sender.css", error: "text cannot be empty" })
-    }
+  if (!text) {
+    return res.render("sender", {
+      csspath: "/css/sender.css",
+      error: "text cannot be empty",
+    });
+  }
 
-    const code = await jsonRepository.insertText(text);
+  const code = await repository.insertText(text);
 
-    return res.render("sender", { csspath: "/css/sender.css", code });
-
+  return res.render("sender", { csspath: "/css/sender.css", code });
 });
-
 
 //Page where users can see shared text
 app.get("/receiver/:code", async (req, res) => {
-
-    const { code } = req.params;
-    const data = await jsonRepository.findTextByCode(code);
-    return res.render("receiver", { csspath: "/css/receiver.css", data });
+  const { code } = req.params;
+  const data = await repository.getText(code);
+  return res.render("receiver", { csspath: "/css/receiver.css", data });
 });
 
 //404 handler
 app.all("*", (_, res, __) => {
-    return res.render("not_found", { csspath: "/css/not_found.css" });
+  return res.render("not_found", { csspath: "/css/not_found.css" });
 });
 
-const port = parsePort(process.env.PORT, 5000);
+const port = parsePort(process.env.PORT, 3000);
 
 app.listen(port, () => {
-    console.log(`Server started listning on http://localhost:${port}/`);
+  console.log(`textism started on port ${port}`);
 });
-
--- src/repository.ts
+++ src/repository.ts
@@ -1,81 +1,41 @@
 import { v4 as uuidv4 } from "uuid";
-import { readFile, writeFile, } from "fs/promises";
-import path from "path";
 import { getExpiryDate } from "./utils";
-import { EventEmitter } from "events";
+import { SetOptions } from "redis";
 
-const DB_LOCATION = path.join(process.cwd(), "data", "db.json");
-
-const DEFAULT_EXPIRY_MINUTES = 5;
+const DEFAULT_EXPIRY_MINUTES = +process.env.EXPIRY_IN_MINUTES!;
 
 export interface SharedText {
-    code: string;
-    text: string;
-    expiry: number;
+  code: string;
+  text: string;
+  expiry: number;
 }
 
-export interface Record {
-    [key: string]: SharedText | undefined
+export interface ICache {
+  set: (key: string, value: string, options?: SetOptions) => Promise<void>;
+  get: (key: string) => Promise<string | null>;
 }
 
-export class JsonTextRepository extends EventEmitter {
+export class Repository {
+  constructor(private readonly cache: ICache) {}
 
-    constructor() {
-        super();
-        this.on("CLEAR_AFTER", this.startTextRemovalJob.bind(this));
-    }
+  async insertText(text: string) {
+    const id = uuidv4();
 
-    private async flush(data: Record) {
-        return writeFile(DB_LOCATION, JSON.stringify(data), { encoding: "utf8" });
-    }
+    const sharedText: SharedText = {
+      code: id,
+      text,
+      expiry: getExpiryDate(DEFAULT_EXPIRY_MINUTES),
+    };
 
-    private async load(): Promise<Record> {
-        const text = await readFile(DB_LOCATION, { encoding: "utf8" }) || "{}";
-        return JSON.parse(text);
-    }
+    await this.cache.set(id, JSON.stringify(sharedText), {
+      EX: DEFAULT_EXPIRY_MINUTES * 60,
+    });
 
-    public async findTextByCode(code: string) {
-        const data = await this.load();
-        return data[code];
-    }
+    return id;
+  }
 
-    public async insertText(text: string): Promise<string> {
-        const code = uuidv4();
-        const newSharedText: SharedText = {
-            code,
-            text,
-            expiry: getExpiryDate(DEFAULT_EXPIRY_MINUTES)
-        };
-        const data = await this.load();
-
-        data[code] = newSharedText;
-        await this.flush(data);
-        this.emit("CLEAR_AFTER", newSharedText);
-        return code;
-    }
-
-    public async removeByCode(code: string) {
-        const data = await this.load();
-        delete data[code];
-        await this.flush(data);
-    }
-
-    private startTextRemovalJob(arg: SharedText) {
-        const { code, expiry } = arg;
-        console.log(`text assosiated with ${code} set to be removed by ${new Date(expiry).toISOString()}`);
-        setTimeout(() => {
-            this.removeByCode(code).then(() => {
-                console.log(`removed text assosiated with code: ${code}`);
-            }).catch(e => {
-                console.log(`couldn't remove text for code ${code}: ${e}`);
-            });
-        }, DEFAULT_EXPIRY_MINUTES * 60 * 1000);
-    }
-
+  async getText(code: string) {
+    const str = await this.cache.get(code);
+    return str ? JSON.parse(str) : null;
+  }
 }
-
-export const jsonRepository = new JsonTextRepository();
-
-
-
-
